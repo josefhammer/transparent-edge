c0288d5100063602b81a1b97c986a902863eafa0
Josef Hammer <josef@hammer.design>
2022-07-28 18:12:50 +0200

    bash-aliases: dfl: Suppress entries for tables 3 and 4.

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -11,4 +11,4 @@ alias ryeport="EDGE_CONFIG='config/edge-single-eport.json' $RYU_EDGE"
 
 alias ry2="EDGE_CONFIG='config/edge-double.json' $RYU_EDGE"
 
-alias dfl="sudo ovs-ofctl dump-flows o-bs1;echo '--';sudo ovs-ofctl dump-flows o-cloudgw"
+alias dfl="sudo ovs-ofctl dump-flows o-bs1 | grep -v 'table=3' | grep -v 'table=4';echo '--';sudo ovs-ofctl dump-flows o-cloudgw"
